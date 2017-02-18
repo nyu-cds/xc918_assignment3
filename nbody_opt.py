9d8b5956ea9187ab5086eba09d40b6f342b33927
xc918 <xc918@nyu.edu>
2017-02-18 14:55:07 -0500

    Global variables changed to local ones.

--- nbody_opt.py
+++ nbody_opt.py
@@ -23,47 +23,6 @@
 
 from itertools import combinations
 
-PI = 3.14159265358979323
-SOLAR_MASS = 4 * PI * PI
-DAYS_PER_YEAR = 365.24
-
-BODIES = {
-    'sun': ([0.0, 0.0, 0.0], [0.0, 0.0, 0.0], SOLAR_MASS),
-
-    'jupiter': ([4.84143144246472090e+00,
-                 -1.16032004402742839e+00,
-                 -1.03622044471123109e-01],
-                [1.66007664274403694e-03 * DAYS_PER_YEAR,
-                 7.69901118419740425e-03 * DAYS_PER_YEAR,
-                 -6.90460016972063023e-05 * DAYS_PER_YEAR],
-                9.54791938424326609e-04 * SOLAR_MASS),
-
-    'saturn': ([8.34336671824457987e+00,
-                4.12479856412430479e+00,
-                -4.03523417114321381e-01],
-               [-2.76742510726862411e-03 * DAYS_PER_YEAR,
-                4.99852801234917238e-03 * DAYS_PER_YEAR,
-                2.30417297573763929e-05 * DAYS_PER_YEAR],
-               2.85885980666130812e-04 * SOLAR_MASS),
-
-    'uranus': ([1.28943695621391310e+01,
-                -1.51111514016986312e+01,
-                -2.23307578892655734e-01],
-               [2.96460137564761618e-03 * DAYS_PER_YEAR,
-                2.37847173959480950e-03 * DAYS_PER_YEAR,
-                -2.96589568540237556e-05 * DAYS_PER_YEAR],
-               4.36624404335156298e-05 * SOLAR_MASS),
-
-    'neptune': ([1.53796971148509165e+01,
-                 -2.59193146099879641e+01,
-                 1.79258772950371181e-01],
-                [2.68067772490389322e-03 * DAYS_PER_YEAR,
-                 1.62824170038242295e-03 * DAYS_PER_YEAR,
-                 -9.51592254519715870e-05 * DAYS_PER_YEAR],
-                5.15138902046611451e-05 * SOLAR_MASS)}
-
-
-Pairwise_name = set(combinations(BODIES,2))
 
 
 def advance(dt, BODIES):
@@ -74,22 +33,22 @@ def advance(dt, BODIES):
     for body1, body2 in Pairwise_name:
         ([x1, y1, z1], v1, m1) = BODIES[body1]
         ([x2, y2, z2], v2, m2) = BODIES[body2]
-        (dx, dy, dz) = (x1-x2, y1-y2, z1-z2)#compute_deltas(x1, x2, y1, y2, z1, z2)
-        #update_vs(v1, v2, dt, dx, dy, dz, m1, m2)
+        (dx, dy, dz) = (x1-x2, y1-y2, z1-z2)#compute_deltas(x1, x2, y1, y2, z1, z2) modified.
+        #update_vs(v1, v2, dt, dx, dy, dz, m1, m2) removed.
         mag = dt * ((dx * dx + dy * dy + dz * dz) ** (-1.5))
         mag_b1 = m1 * mag
         mag_b2 = m2 * mag
-        v1[0] -= dx * mag_b1#compute_b(m2, dt, dx, dy, dz)
-        v1[1] -= dy * mag_b1#compute_b(m2, dt, dx, dy, dz)
-        v1[2] -= dz * mag_b1#compute_b(m2, dt, dx, dy, dz)
-        v2[0] += dx * mag_b2#compute_b(m1, dt, dx, dy, dz)
-        v2[1] += dy * mag_b2#compute_b(m1, dt, dx, dy, dz)
-        v2[2] += dz * mag_b2#compute_b(m1, dt, dx, dy, dz)
+        v1[0] -= dx * mag_b1#compute_b(m2, dt, dx, dy, dz) modified.
+        v1[1] -= dy * mag_b1#compute_b(m2, dt, dx, dy, dz) modified.
+        v1[2] -= dz * mag_b1#compute_b(m2, dt, dx, dy, dz) modified.
+        v2[0] += dx * mag_b2#compute_b(m1, dt, dx, dy, dz) modified.
+        v2[1] += dy * mag_b2#compute_b(m1, dt, dx, dy, dz) modified.
+        v2[2] += dz * mag_b2#compute_b(m1, dt, dx, dy, dz) modified.
         
 
     for body in BODIES.keys():
         (r, [vx, vy, vz], m) = BODIES[body]
-        #update_rs(r, dt, vx, vy, vz)
+        #update_rs(r, dt, vx, vy, vz) removed
         r[0] += dt * vx
         r[1] += dt * vy
         r[2] += dt * vz
@@ -102,8 +61,8 @@ def report_energy(BODIES, e=0.0):
     for body1, body2 in Pairwise_name:
         ([x1, y1, z1], v1, m1) = BODIES[body1]
         ([x2, y2, z2], v2, m2) = BODIES[body2]
-        (dx, dy, dz) = (x1-x2, y1-y2, z1-z2)#compute_deltas(x1, x2, y1, y2, z1, z2)
-        e -= (m1 * m2) / ((dx * dx + dy * dy + dz * dz) ** 0.5)#compute_energy(m1, m2, dx, dy, dz)
+        (dx, dy, dz) = (x1-x2, y1-y2, z1-z2)#compute_deltas(x1, x2, y1, y2, z1, z2) removed.
+        e -= (m1 * m2) / ((dx * dx + dy * dy + dz * dz) ** 0.5)#compute_energy(m1, m2, dx, dy, dz) removed.
     
         
     for body in BODIES.keys():
@@ -145,5 +104,47 @@ def nbody(loops, reference, iterations, BODIES):
         print(report_energy(BODIES))
 
 if __name__ == '__main__':
+    PI = 3.14159265358979323
+    SOLAR_MASS = 4 * PI * PI
+    DAYS_PER_YEAR = 365.24
+
+    BODIES = {
+        'sun': ([0.0, 0.0, 0.0], [0.0, 0.0, 0.0], SOLAR_MASS),
+
+        'jupiter': ([4.84143144246472090e+00,
+                     -1.16032004402742839e+00,
+                     -1.03622044471123109e-01],
+                    [1.66007664274403694e-03 * DAYS_PER_YEAR,
+                     7.69901118419740425e-03 * DAYS_PER_YEAR,
+                     -6.90460016972063023e-05 * DAYS_PER_YEAR],
+                    9.54791938424326609e-04 * SOLAR_MASS),
+
+        'saturn': ([8.34336671824457987e+00,
+                    4.12479856412430479e+00,
+                    -4.03523417114321381e-01],
+                   [-2.76742510726862411e-03 * DAYS_PER_YEAR,
+                    4.99852801234917238e-03 * DAYS_PER_YEAR,
+                    2.30417297573763929e-05 * DAYS_PER_YEAR],
+                   2.85885980666130812e-04 * SOLAR_MASS),
+
+        'uranus': ([1.28943695621391310e+01,
+                    -1.51111514016986312e+01,
+                    -2.23307578892655734e-01],
+                   [2.96460137564761618e-03 * DAYS_PER_YEAR,
+                    2.37847173959480950e-03 * DAYS_PER_YEAR,
+                    -2.96589568540237556e-05 * DAYS_PER_YEAR],
+                   4.36624404335156298e-05 * SOLAR_MASS),
+
+        'neptune': ([1.53796971148509165e+01,
+                     -2.59193146099879641e+01,
+                     1.79258772950371181e-01],
+                    [2.68067772490389322e-03 * DAYS_PER_YEAR,
+                     1.62824170038242295e-03 * DAYS_PER_YEAR,
+                     -9.51592254519715870e-05 * DAYS_PER_YEAR],
+                    5.15138902046611451e-05 * SOLAR_MASS)}
+
+
+    Pairwise_name = set(combinations(BODIES,2))
+
     nbody(100, 'sun', 20000, BODIES)
 
